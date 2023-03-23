72ce917fd30d115bfa514797e4b67ff87511c72f
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-03-23 23:04:31 +0000

    Lucas method

---
+++ Lucas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lucas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lucas(0);
+            Lucas(2);
+            Lucas(7);
+        }
+
+        private static int Lucas(int n)
+        {
+            int total;
+            if (n == 0)
+                total = 2;
+
+            else if (n == 1)
+                total = 1;
+
+            else
+                total = Lucas(n - 1) + Lucas(n - 2);
+
+            Console.WriteLine(total);
+            return total;
+        }
+    }
+}
