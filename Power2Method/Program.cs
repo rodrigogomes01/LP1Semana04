750a7a3d341bc287c577b0cf1455a77f6e8111df
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-03-23 21:57:26 +0000

    Change PowersOfUntil5() to go to the 5th potency of 2

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -6,15 +6,14 @@ namespace Power2Method
     {
         static void Main(string[] args)
         {
+            PowersOf2Until5();
+            PowersOf2Until5();
             PowersOf2Until5(2);
             PowersOf2Until5(7);
         }
         private static void PowersOf2Until5()
         {
-            for (int i = 1; i <= (1 << 5); i = i << 1)
-            {
-                Console.WriteLine(i);
-            }
+            PowersOf2UntilN(5);
         }
 
          private static  void PowersOf2UntilN(int n)
