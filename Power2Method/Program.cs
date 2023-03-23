1dc6b67981a01f1ae5a6c9709e6dbd4d1a43b048
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-03-23 22:10:05 +0000

    Added XML Documentation

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -4,6 +4,7 @@ namespace Power2Method
 {
     class Program
     {
+        ///Calls the main methods used
         static void Main(string[] args)
         {
             PowersOf2Until5();
@@ -11,11 +12,12 @@ namespace Power2Method
             PowersOf2Until5(2);
             PowersOf2Until5(7);
         }
+        ///Calls the method with argument 5.
         private static void PowersOf2Until5()
         {
             PowersOf2UntilN(5);
         }
-
+        ///Prints powers of 2 until n depending of the number given 
          private static  void PowersOf2UntilN(int n)
         {
             for (int i = 1; i <= (1 << n); i = i << 1)
